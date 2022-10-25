f20a5558d4fa5f3ae2424adaf7beeeef0ef6ab22
Markus Laaksonen <malaakso@student.hive.fi>
2022-10-25 18:32:16 +0300

    ft_memmove done, untested, fixed ft_memcpy to read front to back

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,24 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/25 15:53:55 by malaakso          #+#    #+#             */
-/*   Updated: 2022/10/25 18:31:30 by malaakso         ###   ########.fr       */
+/*   Created: 2022/10/25 17:28:26 by malaakso          #+#    #+#             */
+/*   Updated: 2022/10/25 18:30:00 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	size_t	i;
+void		*ft_memcpy(void	*dst, const void *src, size_t n);
 
-	i = 0;
-	while (i < n)
+static void	*ft_memcpyr(void *dst, const void *src, size_t n)
+{
+        while (n > 0)
+        {
+                dst[n - 1] = src[n - 1];
+                n--;
+        }
+        return (dst);
+}
+
+void		*ft_memmove(void *dst, const void *src, size_t len)
+{
+	if (dst < src)
 	{
-		dst[i] = src[i];
-		i++;
+		ft_memcpy(dst, src, len);
+	}
+	else if (dst > src)
+	{
+		ft_memcpyr(dst, src, len);
 	}
 	return (dst);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 15:05:44 by malaakso          #+#    #+#             */
-/*   Updated: 2022/10/25 15:53:43 by malaakso         ###   ########.fr       */
+/*   Updated: 2022/10/25 17:28:15 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ size_t	ft_strlen(const char *s);
 void	*ft_memset(void *b, int c, size_t len);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
