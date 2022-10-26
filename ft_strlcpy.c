c327d2f23a24fb3b77542afff651e9a281a7e732
Markus Laaksonen <malaakso@student.hive.fi>
2022-10-26 17:25:17 +0300

    ft_strlcpy.c done, untested

---
+++ ft_strlcpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/26 15:59:43 by malaakso          #+#    #+#             */
+/*   Updated: 2022/10/26 17:24:11 by malaakso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	len;
+
+	len = 0;
+	while (src[len] != 0)
+		len++;
+	if (dstsize == 0)
+		return (len);
+	i = 0;
+	while (src[i] != 0 && i < dstsize - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = 0;
+	return (len);
+}
