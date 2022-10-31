332f32fc6d7f870273ce2815849cc165636611f6
Markus Laaksonen <malaakso@student.hive.fi>
2022-10-31 18:53:40 +0200

    ft_atoi done and tested

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,31 @@
 /*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 19:44:47 by malaakso          #+#    #+#             */
-/*   Updated: 2022/10/29 19:46:05 by malaakso         ###   ########.fr       */
+/*   Updated: 2022/10/31 18:53:19 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
-	//atoistuff
+	int	ans;
+	int	sign;
+
+	ans = 0;
+	sign = 1;
+	if (!*str)
+		return (0);
+	while ((*str > 8 && *str < 14) || *str == 32)
+		str++;
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		ans = ans * 10 + (*str - '0');
+		str++;
+	}
+	return (ans * sign);
 }
