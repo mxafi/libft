1d1c49df86168e7ea0c1daa505ef05761202ba8e
Markus Laaksonen <malaakso@student.hive.fi>
2022-11-08 17:18:41 +0200

    update ft_calloc, remove protection, test ok

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 15:12:26 by malaakso          #+#    #+#             */
-/*   Updated: 2022/11/08 15:51:56 by malaakso         ###   ########.fr       */
+/*   Updated: 2022/11/08 17:18:09 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,7 @@
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*p;
-	size_t	bytes;
 
-	bytes = count * size;
-	if (count == 0)
-		return (0);
-	if (bytes / count != size)
-		return (0);
 	p = malloc(count * size);
 	if (!p)
 		return (0);
