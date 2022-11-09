97379fdac75a0084e3912f1448c2162c7acfb246
Markus Laaksonen <malaakso@student.hive.fi>
2022-11-09 13:31:28 +0200

    fix ft_calloc added protection

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/01 15:12:26 by malaakso          #+#    #+#             */
-/*   Updated: 2022/11/08 17:18:09 by malaakso         ###   ########.fr       */
+/*   Updated: 2022/11/09 13:30:59 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,15 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*p;
 
+	if (count == 0 || size == 0)
+	{
+		p = malloc(1);
+		if (!p)
+			return (0);
+		return (p);
+	}
+	if (count * size % size)
+		return (0);
 	p = malloc(count * size);
 	if (!p)
 		return (0);
