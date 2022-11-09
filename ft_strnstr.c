28faeece1feae21139f0780a71e584a0f8a1dbd9
Markus Laaksonen <malaakso@student.hive.fi>
2022-11-09 20:02:24 +0200

    hopefully final final commit

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,31 +6,27 @@
 /*   By: malaakso <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 16:16:29 by malaakso          #+#    #+#             */
-/*   Updated: 2022/11/09 19:37:50 by malaakso         ###   ########.fr       */
+/*   Updated: 2022/11/09 20:01:47 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+static char	*ret_found(char *p, const char *needle, size_t len)
 {
-	char	*p;
 	char	*len_addr;
 	size_t	n_len;
+	size_t	p_len;
 
-	p = (char *)haystack;
 	len_addr = p + len - 1;
-	if (*needle == '\0')
-		return (p);
 	n_len = ft_strlen(needle);
-	if (!len)
-		return (0);
 	while (*p && p <= len_addr)
 	{
 		p = ft_strchr(p, needle[0]);
 		if (!p)
-			break ;
-		if (((size_t)len_addr - (size_t)p + 1) >= n_len)
+			return (0);
+		p_len = (size_t)len_addr - (size_t)p + 1;
+		if ((p_len >= n_len) && p_len <= len)
 		{
 			if (!ft_strncmp(p, needle, n_len))
 				return (p);
@@ -38,4 +34,16 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 		p++;
 	}
 	return (0);
+}
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	char	*p;
+
+	p = (char *)haystack;
+	if (*needle == '\0')
+		return (p);
+	if (!len)
+		return (0);
+	return (ret_found(p, needle, len));
 }
