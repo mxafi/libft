3df47d6182ce2eb956105cc62f2e5180c15e1d83
Markus Laaksonen <malaakso@student.hive.fi>
2022-12-15 13:28:56 +0200

    integrate get_next_line into libft

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 17:08:53 by malaakso          #+#    #+#             */
-/*   Updated: 2022/11/21 19:14:55 by malaakso         ###   ########.fr       */
+/*   Updated: 2022/12/15 13:27:39 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 
 # include <stdlib.h>
 # include <unistd.h>
+# include "libft.h"
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1024
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_strdup(const char *s1);
 char	*ft_strndup(const char *s1, size_t n);
-char	*ft_strchr(const char *s, int c);
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
-void	*ft_calloc(size_t count, size_t size);
 
 #endif
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,63 +6,11 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 17:08:44 by malaakso          #+#    #+#             */
-/*   Updated: 2022/11/18 16:20:14 by malaakso         ###   ########.fr       */
+/*   Updated: 2022/12/15 13:26:29 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-void	*ft_calloc(size_t count, size_t size)
-{
-	void			*p;
-	unsigned char	*p_i;
-	size_t			n;
-
-	if (count == 0 || size == 0)
-	{
-		return (ft_calloc(1, 1));
-	}
-	n = count * size;
-	if (n % size || n % count)
-		return (0);
-	p = malloc(n);
-	if (!p)
-		return (0);
-	p_i = p;
-	while (n > 0)
-	{
-		*p_i = 0;
-		p_i++;
-		n--;
-	}
-	return (p);
-}
-
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	src_len;
-	size_t	dst_len;
-
-	src_len = 0;
-	while (src[src_len])
-		src_len++;
-	if (dstsize == 0)
-		return (src_len);
-	dst_len = 0;
-	while (dst[dst_len] && dst_len < dstsize)
-		dst_len++;
-	if (dst_len == dstsize)
-		return (src_len + dstsize);
-	i = dst_len;
-	while (src[i - dst_len] && i < dstsize - 1)
-	{
-		dst[i] = src[i - dst_len];
-		i++;
-	}
-	dst[i] = '\0';
-	return (src_len + dst_len);
-}
 
 char	*ft_strndup(const char *s1, size_t n)
 {
@@ -79,39 +27,5 @@ char	*ft_strndup(const char *s1, size_t n)
 		p[i] = s1[i];
 		i++;
 	}
-	return (p);
-}
-
-char	*ft_strchr(const char *s, int c)
-{
-	char	*p;
-
-	p = (char *)s;
-	while (*p && *p != (char)c)
-		p++;
-	if ((char)c != '\0' && *p == '\0')
-		return (0);
-	return (p);
-}
-
-char	*ft_strdup(const char *s1)
-{
-	size_t	s1_len;
-	char	*p;
-	size_t	i;
-
-	s1_len = 0;
-	while (s1[s1_len])
-		s1_len++;
-	p = malloc(s1_len + 1);
-	if (!p)
-		return (0);
-	i = 0;
-	while (s1[i])
-	{
-		p[i] = s1[i];
-		i++;
-	}
-	p[i] = 0;
 	return (p);
 }
