04e94953a6321a4bd6199bfbba4b43bae41a3b14
Markus Laaksonen <markus@mxa.fi>
2023-07-13 16:56:31 +0300

    add vec_push

--- vec/vec.h
+++ vec/vec.h
@@ -6,7 +6,7 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 13:00:36 by malaakso          #+#    #+#             */
-/*   Updated: 2023/07/13 08:00:50 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/07/13 15:41:19 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,6 @@ int		vec_new(t_vec *dst, size_t init_len, size_t elem_size);
 int		vec_from(t_vec *dst, void *src, size_t len, size_t elem_size);
 int		vec_copy(t_vec *dst, t_vec *src);
 int		vec_resize(t_vec *src, size_t target_size);
+int		vec_push(t_vec *dst, void *src);
 
 #endif
