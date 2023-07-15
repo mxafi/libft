266837770d39a379a680848f83848c0a2e358b5e
Markus Laaksonen <markus@mxa.fi>
2023-07-15 17:14:29 +0300

    add vec to main libft structure

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 15:05:44 by malaakso          #+#    #+#             */
-/*   Updated: 2023/07/09 11:57:36 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/07/15 17:12:27 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include "./basic/libft_basic.h"
 # include "./printf/ft_printf.h"
 # include "./gnl/get_next_line.h"
+# include "./vec/vec.h"
 
 #endif
