210c1c408efff30a5f837d158733cd09e37a2e06
Markus Laaksonen <markus@mxa.fi>
2023-07-15 17:15:08 +0300

    fix norm consecutive new lines

--- gnl/get_next_line.h
+++ gnl/get_next_line.h
@@ -6,10 +6,9 @@
 /*   By: malaakso <malaakso@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 17:08:53 by malaakso          #+#    #+#             */
-/*   Updated: 2023/07/09 12:03:18 by malaakso         ###   ########.fr       */
+/*   Updated: 2023/07/15 17:14:47 by malaakso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
